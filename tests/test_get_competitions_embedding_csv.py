cfc3d28836eb4df65b4185720683f6afb802c3d6
rma <rma@infologic.fr>
2023-09-25 17:55:59 +0200

    adding first unit test

--- tests/test_get_competitions_embedding_csv.py
+++ tests/test_get_competitions_embedding_csv.py
@@ -1,7 +1,7 @@
-
 def test_get_competitions_embedding_csv_simple():
     # we want to download the csv from kaggle, scrap each competition description, transform them as correct text, embed them to finally store them in the csv
-    df = get_competitions_embedding_csv()
-    
-    assert "embedding" in df 
-    
+    df = get_competitions_embedding_csv(StubKaggleDownloader(), StubCompetitionScrapper(), StubLLM(), StubCSVWriter())
+
+    assert "embedding" in df
+    assert len(df) > 0
+    assert all(isinstance(item, list) for item in df["embedding"])
